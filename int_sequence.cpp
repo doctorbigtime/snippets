72d7c12f95dced752ef87d4b4d870717c63500ec
Sebastien Fortas <sebastien@fortas.org>
2017-04-19 19:05:52 -0400

    added some comments to int_sequence

--- int_sequence.cpp
+++ int_sequence.cpp
@@ -1,28 +1,28 @@
+// how to generate a monotonically increasing integer sequence as template parameters.
 #include <iostream>
 
+// here's the integer sequence.
 template <int...>
 struct int_sequence
 {
 
 };
 
+// basic template that gets called like this make_int_sequence<5>
 template <int N, int... Is>
 struct make_int_sequence : make_int_sequence<N - 1, N - 1, Is...>
 {
 };
 
+// specialization to stop the iteration.
 template <int... Is>
 struct make_int_sequence<0, Is...> : int_sequence<Is...>
 {
 };
 
-void print(int_sequence<>) {}
-
 template <int...Is> 
 void print(int_sequence<Is...>)
 {
-    //std::cout << "N: " << N << std::endl;
-    //print(int_sequence<Is...>{});
     std::cout << "print:\n";
     int dummy[] = { (std::cout << Is << '\n', 0)... };
 }
